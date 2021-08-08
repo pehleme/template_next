81dd831867a79115c3213fe2f2073856b6ed9e0f
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2021-08-08 01:44:30 -0300

    Update chackra ui

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import { ReactElement } from 'react';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from 'components/layout';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import 'styles/globals.css';
+import { theme } from '~/styles/theme';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
