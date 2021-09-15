b629c8685dfe3dcccb9d3a06e801c766b400e525
Pedro Leme <pedro_leme98@hotmail.com>
2021-09-15 13:23:23 -0300

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,9 +5,7 @@ import { Flex } from '@chakra-ui/react';
 const Layout: React.FC = ({ children }) => {
   return (
     <Flex height="100vh" width="100vw">
-      <Flex flex="1" maxW="1056px" w="100%" mt="28" pb="8" px="8">
-        {children}
-      </Flex>
+      {children}
     </Flex>
   );
 };
